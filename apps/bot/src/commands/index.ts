5f8dd90a56fa12284968d382b4da24bf85b752a8
Amir <78366053+yungamiro@users.noreply.github.com>
2026-09-12 18:27:11 +0200

    register standard music bot commands

--- apps/bot/src/commands/index.ts
+++ apps/bot/src/commands/index.ts
@@ -1,9 +1,26 @@
 import type { BotCommand } from "../types.js";
 import { joinCommand } from "./join.js";
 import { leaveCommand } from "./leave.js";
+import { nowPlayingCommand } from "./nowplaying.js";
+import { pauseCommand } from "./pause.js";
 import { pingCommand } from "./ping.js";
 import { playCommand } from "./play.js";
+import { queueCommand } from "./queue.js";
+import { resumeCommand } from "./resume.js";
+import { skipCommand } from "./skip.js";
 import { stopCommand } from "./stop.js";
 
-export const commands: BotCommand[] = [pingCommand, joinCommand, leaveCommand, playCommand, stopCommand];
+export const commands: BotCommand[] = [
+  pingCommand,
+  joinCommand,
+  leaveCommand,
+  playCommand,
+  pauseCommand,
+  resumeCommand,
+  skipCommand,
+  stopCommand,
+  queueCommand,
+  nowPlayingCommand
+];
+
 export const commandMap = new Map(commands.map((command) => [command.data.name, command]));
